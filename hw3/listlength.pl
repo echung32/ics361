56e8177e4a3b851c4f9b4c0fd258f1183cb4af18
echung32 <143231894+echung32@users.noreply.github.com>
2024-11-05 10:18:30 +0000

    fix list length

--- hw3/listlength.pl
+++ hw3/listlength.pl
@@ -22,14 +22,33 @@
 %     Len is Incr + 1.
 
 % - USING DISTINCT STATEMENTS
+% cuts added to prevent backtracking for input handling
+% i.e. check_len fails, check_list will also print failure
+% even though it's perfectly valid. cut to prevent this.
+% https://homepage.divms.uiowa.edu/~hzhang/c188/notes/ch08b-cut.pdf
+
 listlength(List, Len) :-
-    check_list(List),
+    check_list(List), !,
+    check_len(Len),
     listlength_helper(List, Len).
 
 listlength_helper([], 0).
 listlength_helper([_|Rem], Len) :-
     listlength_helper(Rem, Incr),
     Len is Incr + 1.
+
+% is an integer, pass.
+check_len(Len) :-
+    integer(Len), !.
+
+% not yet instantiated, pass.
+check_len(Len) :-
+    var(Len), !.
+
+% Otherwise, not an integer.
+check_len(_) :-
+    print("LISTLENGTH/2: The second argument must be an integer."), 
+    !, fail.
 
 % An empty list is a valid list.
 check_list([]).
